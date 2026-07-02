5421242d12c061c9d1a12e531780155780027fc2
Quý Anh <anh.lequy2706@hcmut.edu.vn>
2026-07-02 23:00:35 +0700

    week01: hoan thanh bai tap buoi 1

--- week01/main.c
+++ week01/main.c
@@ -7,9 +7,10 @@
 
 void parse_config(const uint8_t *config_packet, int16_t *high_threshold) {
     // HỌC VIÊN BẮT ĐẦU VIẾT CODE TỪ ĐÂY
-
-
-
+    if (config_packet == NULL || high_threshold == NULL) {
+        return;
+    }
+*high_threshold = (int16_t)((config_packet[1] << 8) | config_packet[0]);
 
     // HỌC VIÊN KẾT THÚC VIẾT CODE
 }
@@ -18,7 +19,11 @@ void parse_config(const uint8_t *config_packet, int16_t *high_threshold) {
 
 int16_t read_temperature_reg(void *hw_sensor_reg) {
     // HỌC VIÊN BẮT ĐẦU VIẾT CODE TỪ ĐÂY
-
+if (hw_sensor_reg == NULL) {
+    return 0;
+}
+volatile int16_t *temp_reg = (volatile int16_t *)hw_sensor_reg;
+return *temp_reg;
 
 
 
@@ -29,9 +34,18 @@ int16_t read_temperature_reg(void *hw_sensor_reg) {
 
 void control_output(uint8_t *control_reg, uint8_t fan_enable, uint8_t alarm_enable) {
     // HỌC VIÊN BẮT ĐẦU VIẾT CODE TỪ ĐÂY
+if (control_reg == NULL) {
+    return;
 
-
-
+}
+*control_reg &= ~(0x03);
+if (fan_enable == 1)
+{
+    *control_reg |= (1<<0)
+}
+if (alarm_enable ==1 ) {
+    *control_reg |= (1<<1);
+}
 
     // HỌC VIÊN KẾT THÚC VIẾT CODE
 }
